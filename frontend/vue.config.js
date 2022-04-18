356d873bc4e5bb95f57be73ed163a81b9c53fd42
Stephanie Sam <ponn.stephanie@gmail.com>
2022-04-18 12:49:12 +0100

    deployment

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: ['vuetify'],
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/P7-groupomania/'
+    : '/'
+}
