f83d04039667f11f306e99ca2abed61d90ea9d77
Stephanie Sam <ponn.stephanie@gmail.com>
2022-04-18 12:57:02 +0100

    deployment vue

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:raviiidev/P7-groupomania.git main:gh-pages
+git push -f git@github.com:raviiidev/P7-groupomania.git main:P7-groupomania
 
 cd -
